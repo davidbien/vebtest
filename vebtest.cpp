f58412d7a496a1f01fa008a74f85660d5293633a
David Bien <davidbien@hotmail.com>
2020-06-13 02:28:12 -0600

    Working on VebTree unit testing.

--- vebtest.cpp
+++ vebtest.cpp
@@ -3,11 +3,14 @@
 // Test Van Emde Boas tree stuff.
 // dbien: 11JUN2020
 
+#include <stdlib.h>
 #include <thread>
+#include <random>
 #include "syslogmgr.h"
 #include "jsonstrm.h"
 #include "jsonobjs.h"
 #include "_strutil.h"
+#include "_simpbv.h"
 #include "_vebtree.h"
 #include "syslogmgr.inl"
 
@@ -18,7 +21,7 @@ __BIENUTIL_USING_NAMESPACE
 int
 TryMain( int argc, char *argv[] )
 {
-#define USAGE "Usage: %s"
+#define USAGE "Usage: %s [percentage populated(1-1000)=30][random seed]"
     g_strProgramName = *argv;
     n_SysLog::InitSysLog( argv[0], LOG_PERROR, LOG_USER );
 #if 0
@@ -30,29 +33,56 @@ TryMain( int argc, char *argv[] )
     typedef VebTreeVariable< 256 > _tyVebTreeSummary;
     typedef VebTreeVariable< 65536, _tyVebTreeSummary > _tyVebTree64;
     _tyVebTree64 veb;
-    veb.Init( 10000000 );
-    const size_t stUniverse = 10000000;
+    const size_t stUniverse = 1000000;
+    veb.Init( stUniverse );
 #endif
+    uint32_t nRandSeed = time(0);
+    if ( argc > 2 )
+         nRandSeed = atoi( argv[2] );
+    n_SysLog::Log( eslmtInfo, "%s: iRandSeed[%d]", g_strProgramName.c_str(), nRandSeed );
+
+    uint32_t nPercentPop = 30;
+    if ( argc > 1 )
+        nPercentPop = atoi( argv[1] );
+    if ( nPercentPop > 1000 )
+        nPercentPop = 1000;
+    n_SysLog::Log( eslmtInfo, "%s: nPercentPop[%u/1000]", g_strProgramName.c_str(), nPercentPop );
+
     for ( size_t stCur = 0; stCur < stUniverse; ++stCur )
     {
-        if ( 31 == stCur )
-        {
-            int Z = 1;
-            ++Z;
-        }
         veb.Insert( stCur );
     }
     for ( size_t stCur = 0; stCur < stUniverse; ++stCur )
     {
-        if ( 63 == stCur )
-        {
-            int Z = 1;
-            ++Z;
-        }
         veb.Delete( stCur );
     }
     assert( !veb.FHasAnyElements() );
-    return 0;
+    assert( veb.FEmpty( true ) ); // Recursively ensure emptiness.
+
+    // Now test successor and predecessor.
+    typedef _simple_bitvec< size_t, allocator< size_t > > _tyBV;
+
+    // Get a default random generator and seed it:
+    std::default_random_engine rneDefault( nRandSeed );
+    std::uniform_int_distribution< size_t > sidDist( 0, stUniverse-1 );
+    auto genRand = std::bind( sidDist, rneDefault );
+
+    _tyBV bvMirror( stUniverse );
+
+    size_t nElementsGenerate = ( stUniverse * nPercentPop ) / 1000;
+    size_t nInserted = 0;
+    while( nElementsGenerate-- )
+    {
+        size_t st = genRand();
+        bvMirror.setbit( st );
+        nInserted += veb.FCheckInsert( st );
+    }
+    n_SysLog::Log( eslmtInfo, "%s: nInserted[%ul]", g_strProgramName.c_str(), nInserted );
+
+    // Make some copies so we can reuse them:
+    
+
+   return 0;
 }
 
 int
