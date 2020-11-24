305f5f619d530612dd578365e8053ec4ea12586f
David Bien <davidbien@hotmail.com>
2020-11-24 00:12:11 -0700

    Propagate new Assert() macro..

--- vebtest.cpp
+++ vebtest.cpp
@@ -8,6 +8,7 @@
 #include <random>
 #include <chrono>
 #include "syslogmgr.h"
+#include "_assert.h"
 #include "jsonstrm.h"
 #include "jsonobjs.h"
 #include "_strutil.h"
@@ -130,7 +131,7 @@ TryMain( int argc, char *argv[] )
     _tyVebTree vebAll( stUniverse );
     vebAll.InsertAll();
     vebAll.AssertValid();
-    assert( vebAll == veb );
+    Assert( vebAll == veb );
 
     // Make some copies so we can reuse them:
     _tyVebTree vebCopy( veb );
@@ -142,13 +143,13 @@ TryMain( int argc, char *argv[] )
     for ( size_t stCur = 0; stCur < stUniverse; ++stCur )
     {
         bool fDeleted = vebCopy2.FCheckDelete( stCur );
-        assert( fDeleted );
+        Assert( fDeleted );
     }
     vebCopy2.AssertValid();
-    assert( !vebCopy2.FHasAnyElements() );
-    assert( vebCopy2.FEmpty( true ) ); // Recursively ensure emptiness.
+    Assert( !vebCopy2.FHasAnyElements() );
+    Assert( vebCopy2.FEmpty( true ) ); // Recursively ensure emptiness.
     vebCopy2.swap( veb );
-    assert( veb.FEmpty( true ) ); // Recursively ensure emptiness.
+    Assert( veb.FEmpty( true ) ); // Recursively ensure emptiness.
 
     // Now test successor and predecessor.
     typedef _simple_bitvec< size_t, allocator< size_t > > _tyBV;
@@ -173,7 +174,7 @@ TryMain( int argc, char *argv[] )
     veb.AssertValid();
     n_SysLog::Log( eslmtInfo, "%s: nInserted[%lu]", g_strProgramName.c_str(), nInserted );
     uint64_t nSetBitsBefore = bvMirror.countsetbits();
-    assert( nInserted == nSetBitsBefore );
+    Assert( nInserted == nSetBitsBefore );
 
     {//B - test bitwise functions:
         _tyVebTree vebInvert( veb );
@@ -181,36 +182,36 @@ TryMain( int argc, char *argv[] )
         vebInvert.AssertValid();
         vebInvert &= veb;
         vebInvert.AssertValid();
-        assert( vebInvert.FEmpty( true ) );
+        Assert( vebInvert.FEmpty( true ) );
         vebInvert |= veb;
-        assert( vebInvert == veb );
+        Assert( vebInvert == veb );
         vebInvert.BitwiseInvert();
         vebInvert ^= veb; // This should make vebInvert fully occupied.
         vebInvert.AssertValid();
         vebInvert.BitwiseInvert();
-        assert( vebInvert.FEmpty( true ) );
+        Assert( vebInvert.FEmpty( true ) );
     }//EB
 
     vebCopy2 = veb;
-    assert( vebCopy2 == veb );
-    assert( veb == vebCopy2 );
+    Assert( vebCopy2 == veb );
+    Assert( veb == vebCopy2 );
     vebCopy.Clear();
     vebCopy.swap( vebCopy2 );
-    assert( vebCopy2.FEmpty( true ) );
+    Assert( vebCopy2.FEmpty( true ) );
     _tyVebTree vebCopy3( std::move( vebCopy ) );
     //vebCopy2 = std::move( vebCopy );
-    assert( vebCopy.FEmpty( true ) );
-    assert( vebCopy3 == veb );
-    assert( veb == vebCopy3 );
+    Assert( vebCopy.FEmpty( true ) );
+    Assert( vebCopy3 == veb );
+    Assert( veb == vebCopy3 );
 
     vebCopy2 = std::move( vebCopy3 );
-    assert( vebCopy3.FEmpty( true ) );
-    assert( vebCopy2 == veb );
+    Assert( vebCopy3.FEmpty( true ) );
+    Assert( vebCopy2 == veb );
 
     // Now let's actually get to the successor and predecessor testing:
     _tyBV bvMirrorCopy( bvMirror );
     uint64_t nSetBitsBeforeCopy = bvMirrorCopy.countsetbits();
-    assert( nSetBitsBeforeCopy == nSetBitsBefore );
+    Assert( nSetBitsBeforeCopy == nSetBitsBefore );
     _tyBV bvMirrorCopy2( bvMirror );
 
     { //B: Successor:
@@ -220,13 +221,13 @@ TryMain( int argc, char *argv[] )
         size_t nSuccessor;
         while( _tyVebTree::s_kitNoSuccessor != ( nSuccessor = veb.NSuccessor( nCurEl ) ) )
         {
-            assert( ( nCurEl == numeric_limits< size_t >::max() ) || ( nSuccessor > nCurEl ) );
-            assert( bvMirror.isbitset( nSuccessor ) );
+            Assert( ( nCurEl == numeric_limits< size_t >::max() ) || ( nSuccessor > nCurEl ) );
+            Assert( bvMirror.isbitset( nSuccessor ) );
             bvMirror.clearbit( nSuccessor );
             nCurEl = nSuccessor;
         }
         uint64_t nSetBitsAfter = bvMirror.countsetbits();
-        assert( !nSetBitsAfter );
+        Assert( !nSetBitsAfter );
     } //EB
 
     { //B: Predecessor:
@@ -238,13 +239,13 @@ TryMain( int argc, char *argv[] )
         size_t nthCall = 0;
         while( _tyVebTree::s_kitNoPredecessor != ( nPredecessor = veb.NPredecessor( nCurEl ) ) )
         {
-            assert( nPredecessor < nCurEl );
-            assert( bvMirrorCopy.isbitset( nPredecessor ) );
+            Assert( nPredecessor < nCurEl );
+            Assert( bvMirrorCopy.isbitset( nPredecessor ) );
             bvMirrorCopy.clearbit( nPredecessor );
             nCurEl = nPredecessor;
             ++nthCall;
         }
-        assert( !bvMirrorCopy.countsetbits() );
+        Assert( !bvMirrorCopy.countsetbits() );
     } //EB
 
     _tyBV bvMirrorCopy3( bvMirrorCopy2 );
@@ -257,14 +258,14 @@ TryMain( int argc, char *argv[] )
         size_t nSuccessor;
         while( _tyVebTree::s_kitNoSuccessor != ( nSuccessor = vebSuccessorDelete.NSuccessorDelete( nCurEl ) ) )
         {
-            assert( ( nCurEl == numeric_limits< size_t >::max() ) || ( nSuccessor > nCurEl ) );
-            assert( bvMirrorCopy2.isbitset( nSuccessor ) );
+            Assert( ( nCurEl == numeric_limits< size_t >::max() ) || ( nSuccessor > nCurEl ) );
+            Assert( bvMirrorCopy2.isbitset( nSuccessor ) );
             bvMirrorCopy2.clearbit( nSuccessor );
             nCurEl = nSuccessor;
         }
         uint64_t nSetBitsAfter = bvMirrorCopy2.countsetbits();
-        assert( !nSetBitsAfter );
-        assert( vebSuccessorDelete.FEmpty( true ) );
+        Assert( !nSetBitsAfter );
+        Assert( vebSuccessorDelete.FEmpty( true ) );
     } //EB
 
     { //B: PredecessorDelete:
@@ -277,13 +278,13 @@ TryMain( int argc, char *argv[] )
         size_t nPredecessor;
         while( _tyVebTree::s_kitNoPredecessor != ( nPredecessor = vebPredecessorDelete.NPredecessorDelete( nCurEl ) ) )
         {
-            assert( nPredecessor < nCurEl );
-            assert( bvMirrorCopy3.isbitset( nPredecessor ) );
+            Assert( nPredecessor < nCurEl );
+            Assert( bvMirrorCopy3.isbitset( nPredecessor ) );
             bvMirrorCopy3.clearbit( nPredecessor );
             nCurEl = nPredecessor;
         }
-        assert( !bvMirrorCopy3.countsetbits() );
-        assert( vebPredecessorDelete.FEmpty( true ) );
+        Assert( !bvMirrorCopy3.countsetbits() );
+        Assert( vebPredecessorDelete.FEmpty( true ) );
     } //EB
 
     veb.Resize( stUniverse /= 2, true );
@@ -293,7 +294,7 @@ TryMain( int argc, char *argv[] )
     {
         (void)veb.FCheckDelete( stCur );
     }
-    assert( veb.FEmpty( true ) );
+    Assert( veb.FEmpty( true ) );
 
     return 0;
 }
